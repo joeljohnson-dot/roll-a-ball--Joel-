215e4bc5479e729310337d70a55b1e1b2e39290c
Liam Wetherald <liam.wetherald@discoverystudent.org>
2025-11-03 08:52:52 -0500

    Hii
    using UnityEngine;
    using unityEngine.InputSystem;
    public class PLAYERCONTROLLER : MonoBehaviour
    {
        // Start is called once before the first execution of Update after the MonoBehaviour is created
        void Start()
        {
    
        }
    
        // Update is called once per frame
        void Update()
        {
    
        }
    }

---
+++ Assets/PLAYERCONTROLLER.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using unityEngine.InputSystem;
+public class PLAYERCONTROLLER : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
