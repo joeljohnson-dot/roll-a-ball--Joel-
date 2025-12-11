c4901a8634ef08ceb1c266a31425cb189a798e94
joeljohnson-dot <joel.johnson@discoverystudent.org>
2025-12-11 10:00:52 -0500

    vcx

---
+++ Assets/Scripts/PLAYERCONTROLLER.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using Systems.Collections.Generic;
+using UnityEngine;
+
+
+public class PLAYERCONTROLLER : MonoBehaviour
+get 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void OnMove()
+    {
+        
+    }
+}
