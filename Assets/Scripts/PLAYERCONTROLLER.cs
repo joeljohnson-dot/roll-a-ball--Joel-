9780f5b98af42f8c48aa2b58e46f8771d87409fb
joeljohnson-dot <joel.johnson@discoverystudent.org>
2025-12-15 08:52:43 -0500

    New Scripting yea

--- Assets/Scripts/PLAYERCONTROLLER.cs
+++ Assets/Scripts/PLAYERCONTROLLER.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using Systems.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0; 
 
-public class PLAYERCONTROLLER : MonoBehaviour
-get 
+    private Rigidbody rb; 
+    private float movementX;
+    private float movementY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent <Rigidbody>(); 
+    }
            
-    }
 
-    // Update is called once per frame
-    void OnMove()
-    {
-        
-    }
+ void OnMove (InputValue movementValue)
+   {
+   Vector2 movementVector = movementValue.Get<Vector2>(); 
+
+   movementX = movementVector.x; 
+   movementY = movementVector.y; 
+   }
+
+   private void FixedUpdate() 
+   {
+    Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+   rb.AddForce(movement  * speed); 
+   }
 }
